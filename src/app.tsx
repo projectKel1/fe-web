35027e5cc75b0f9512b00126b41fe03ff19dfc4f
ImamBaihaqi <imam.baihaqi2396@gmail.com>
2023-09-13 23:10:58 +0700

    add file landing and login

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Layout from "./components/layout"
-import Dashboard from "./pages/dashboard/dashboardManager"
-import { Toaster } from "react-hot-toast"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/layout';
+import Dashboard from './pages/dashboard/dashboardManager';
+import Landing from './pages/landing/landing';
+import Login from './pages/login/login';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-      />
+      <Toaster position="top-center" reverseOrder={false} />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Layout />}>
             <Route path="/dashboard" element={<Dashboard />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/landing/landing.tsx
@@ -0,0 +1,19 @@
+const landing = () => {
+  return (
+    <div>
+      <img
+        src="https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+        alt=""
+      />
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dolores
+          optio adipisci obcaecati soluta quasi!
+        </p>
+        <button>Get Startet</button>
+      </div>
+    </div>
+  );
+};
+
+export default landing;
