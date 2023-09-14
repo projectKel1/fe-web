43f96475764fc96191da97aad3e38094801a709f
ImamBaihaqi <imam.baihaqi2396@gmail.com>
2023-09-14 21:08:50 +0700

    feat: slicing reimbursment employee page

---
+++ src/pages/reimbursement/reimbursmentEmployee.tsx
@@ -0,0 +1,27 @@
+import CardReimbursement from '../../components/cardReimbursement';
+import Table from '../../components/table';
+
+const ReimbursmentEmployee = () => {
+  return (
+    <div className="p-10 h-full bg-bgMain">
+      <div className="py-2">
+        <CardReimbursement
+          tittle="Reimbursement Manager"
+          subTittle="Check Reimbursement here"
+        />
+      </div>
+      <div className="py-2 w-full bg-white rounded-cardBase">
+        <div className="p-5">
+          <h1 className="font-semibold underline-offset-8 underline">
+            History Reimbursement
+          </h1>
+        </div>
+        <div className="p-2">
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReimbursmentEmployee;
