56891ecfaa42e53ac70ec35aa0126145db048132
ImamBaihaqi <imam.baihaqi2396@gmail.com>
2023-09-18 12:24:00 +0700

    fix: fix slicing login page

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,17 @@
 interface Button {
-    textBtn: string | any
-    color: string
-    onClick: any
+  textBtn: string | any;
+  color: string;
+  onClick: any;
 }
-const Button: React.FC<Button> = ({ textBtn,color,onClick }) => {
-    return (
-        <button onClick={onClick} className={`${color} px-10 py-4  hover:shadow-lg hover:text-opacity-90 font-semibold text-white rounded-btn w-56 flex justify-center items-center`}>
-            {textBtn}
-        </button>
-    )
-}
+const Button: React.FC<Button> = ({ textBtn, color, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${color} px-10 py-4  hover:shadow-lg hover:text-opacity-90 font-semibold text-white rounded-btn w-56 flex justify-center items-center w-full`}
+    >
+      {textBtn}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,70 +1,84 @@
-import { useFormik } from "formik";
-import { LoginAuth } from "../../auth/yup";
-import axios from "axios";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
-import { useState, useEffect } from "react";
+import { useFormik } from 'formik';
+import { LoginAuth } from '../../auth/yup';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { useState, useEffect } from 'react';
 import { LuEye, LuEyeOff, LuLoader } from 'react-icons/lu';
-import Button from "../../components/button";
+import Button from '../../components/button';
 
 const Login = () => {
   const [status, setStatus] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const formik = useFormik({
     initialValues: {
-      email: "",
-      password: ""
-    }, validationSchema: LoginAuth,
+      email: '',
+      password: '',
+    },
+    validationSchema: LoginAuth,
     onSubmit: (values) => {
-      setStatus(true)
-      axios.post(`/login`, {
-        email: values.email,
-        password: values.password
-      }).then((response) => {
-        toast.success(response.data.message)
-        Cookies.set('data', JSON.stringify(response.data))
-        if (response.data.data.role_id === 1) {
-          navigate('/dashboard-manager')
-        }
-        if (response.data.data.role_id == 2) {
-          navigate('/dashboard-hr')
-        }
-        setStatus(false)
-      }).catch((error) => {
-        if (error.response && error.response.status === 500) {
-          toast.error('Email atau password salah');
-          setStatus(false)
-        } else {
-          toast.error('Server tidak merespon. Mohon coba lagi nanti.');
-          setStatus(false)
-        }
-      })
-    }
-  })
+      setStatus(true);
+      axios
+        .post(`/login`, {
+          email: values.email,
+          password: values.password,
+        })
+        .then((response) => {
+          toast.success(response.data.message);
+          Cookies.set('data', JSON.stringify(response.data));
+          if (response.data.data.role_id === 1) {
+            navigate('/dashboard-manager');
+          }
+          if (response.data.data.role_id == 2) {
+            navigate('/dashboard-hr');
+          }
+          setStatus(false);
+        })
+        .catch((error) => {
+          if (error.response && error.response.status === 500) {
+            toast.error('Email atau password salah');
+            setStatus(false);
+          } else {
+            toast.error('Server tidak merespon. Mohon coba lagi nanti.');
+            setStatus(false);
+          }
+        });
+    },
+  });
 
   useEffect(() => {
     if (Cookies.get('data')) {
-      const tempData: any = Cookies.get('data')
-      const Data = JSON.parse(tempData)
+      const tempData: any = Cookies.get('data');
+      const Data = JSON.parse(tempData);
       if (Data.data.role_id === 1) {
-        navigate('/dashboard-manager')
+        navigate('/dashboard-manager');
       }
     }
-  }, [])
+  }, []);
 
   return (
     <section>
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen md:h-screen bg-bgMain">
-        <div className="w-full bg-white rounded-lg shadow-lg md:mt-0 sm:max-w-md xl:p-0">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+      <div className="grid grid-cols-2  px-6 py-8 mx-auto h-screen md:h-screen bg-bgMain">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-4xl font-bold">Hi, Welcome Back</h1>
+          <img
+            src="https://minimals.cc/assets/illustrations/illustration_dashboard.png"
+            alt=""
+          />
+        </div>
+        <div className="flex justify-center items-center  ">
+          <div className="p-6 space-y-4 w-96 md:space-y-6 sm:p-8">
             <div className="space-y-3">
-              <h1 className="text-xl flex justify-center  font-bold leading-tight tracking-tightmd:text-2xl">
-                Login
+              <h1 className="text-2xl  font-semibold leading-tight tracking-tight md:text-2xl">
+                Sign in to HRIS App
               </h1>
             </div>
-            <form className="space-y-4 md:space-y-8" onSubmit={formik.handleSubmit}>
+            <form
+              className="space-y-4 md:space-y-8"
+              onSubmit={formik.handleSubmit}
+            >
               <div>
                 <label className=" mb-2 text-sm font-semibold ">Email</label>
                 <input
@@ -73,11 +87,17 @@ const Login = () => {
                   onBlur={formik.handleBlur}
                   onChange={formik.handleChange}
                   autoComplete="off"
-                  className={`bg-gray-50 border ${formik.touched.email && formik.errors.email ? `border-red-800` : 'border-gray-300 border-2'} border-gray-300 text-gray-900  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                  className={`bg-gray-50 border ${
+                    formik.touched.email && formik.errors.email
+                      ? `border-red-800`
+                      : 'border-gray-300 border-2'
+                  } border-gray-300 text-gray-900  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
                   placeholder="email@email.com"
                 />
                 {formik.touched.email && formik.errors.email ? (
-                  <div className='text-red-800 absolute focus:outline-red-800 text-sm font-semibold'>{formik.errors.email}</div>
+                  <div className="text-red-800 absolute focus:outline-red-800 text-sm font-semibold">
+                    {formik.errors.email}
+                  </div>
                 ) : null}
               </div>
               <div>
@@ -88,23 +108,33 @@ const Login = () => {
                     name="password"
                     onBlur={formik.handleBlur}
                     onChange={formik.handleChange}
-                    className={`bg-gray-50 border relative ${formik.touched.email && formik.errors.email ? `border-red-800` : 'border-gray-300 border-2'} border-gray-300 text-gray-900  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
+                    className={`bg-gray-50 border relative ${
+                      formik.touched.email && formik.errors.email
+                        ? `border-red-800`
+                        : 'border-gray-300 border-2'
+                    } border-gray-300 text-gray-900  rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5`}
                     placeholder="••••••••"
                   />
                   <button
                     type="button"
-                    className="absolute right-4 top-2/4 transform -translate-y-1/2 cursor-pointer"
+                    className=" absolute right-4 top-2/4 transform -translate-y-1/2 cursor-pointer"
                     onClick={() => setShowPassword(!showPassword)}
                   >
                     {showPassword ? <LuEye /> : <LuEyeOff />}
                   </button>
                   {formik.touched.password && formik.errors.password ? (
-                    <div className='text-red-800 absolute text-sm font-semibold'>{formik.errors.password}</div>
+                    <div className="text-red-800 absolute text-sm font-semibold">
+                      {formik.errors.password}
+                    </div>
                   ) : null}
                 </div>
               </div>
-              <div className="flex justify-center">
-                <Button textBtn={status ? <LuLoader /> : "Login"} onClick={null} color="bg-bgBtn" />
+              <div className="">
+                <Button
+                  textBtn={status ? <LuLoader /> : 'Login'}
+                  onClick={null}
+                  color="bg-bgBtn"
+                />
               </div>
             </form>
           </div>
